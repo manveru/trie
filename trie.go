896916dddf66f6d77f966eb4a486a442c2e22364
Michael Fellinger <m.fellinger@gmail.com>
2012-04-15 13:34:36 +0200

    add a few more functions

--- trie.go
+++ trie.go
@@ -1,6 +1,11 @@
+// Ternary Search Tree
 package trie
 
-// naming chosen to align, not to be meaningful :P
+// An empty value of this type can be used to Put values.
+type Trie struct {
+	root *node
+}
+
 type node struct {
 	small *node
 	equal *node
@@ -10,10 +15,9 @@ type node struct {
 	end   bool
 }
 
-type Trie struct {
-	root *node
-}
-
+// Put adds a key/value pair to the Trie.
+//
+// Complexity: O(m)
 func (t *Trie) Put(key string, value interface{}) {
 	if len(key) < 1 {
 		return
@@ -22,6 +26,9 @@ func (t *Trie) Put(key string, value interface{}) {
 	t.root = t.putRecursive(t.root, []rune(key), 0, value)
 }
 
+// Get returns the value of the desired key, or nil if the key wasn't found.
+//
+// Complexity: O(m) worst case
 func (t *Trie) Get(key string) interface{} {
 	if len(key) < 1 {
 		return nil
@@ -32,6 +39,82 @@ func (t *Trie) Get(key string) interface{} {
 		return nil
 	}
 	return node.value
+}
+
+// Wildcard Returns a sorted slice with matches for the key.
+// The wildcard characters that match any character are '*' and '.'.
+// If no match is found, an empty slice is returned.
+//
+// Complexity: O(n) worst case
+func (t *Trie) Wildcard(key string) []string {
+	if len(key) < 1 {
+		return nil
+	}
+
+	return t.wildcardRecursive(t.root, []rune(key), 0, "")
+}
+
+// LongestPrefix returns the longest key that has a prefix in common with the key.
+// If no match is found, "" is returned.
+//
+// Complexity: O(m) worst case
+func (t *Trie) LongestPrefix(key string) string {
+	if len(key) < 1 {
+		return ""
+	}
+
+	length := t.prefixRecursive(t.root, []rune(key), 0)
+	return key[0:length]
+}
+
+func (t *Trie) prefixRecursive(n *node, key []rune, index int) int {
+	if n == nil || index == len(key) {
+		return 0
+	}
+
+	length := 0
+	recLen := 0
+	r := key[index]
+
+	if r < n.r {
+		recLen = t.prefixRecursive(n.small, key, index)
+	} else if r > n.r {
+		recLen = t.prefixRecursive(n.large, key, index)
+	} else {
+		if n.end {
+			length = index + 1
+		}
+		recLen = t.prefixRecursive(n.equal, key, index+1)
+	}
+	if length > recLen {
+		return length
+	}
+	return recLen
+}
+
+func (t *Trie) wildcardRecursive(n *node, key []rune, index int, prefix string) (matches []string) {
+	if n == nil || index == len(key) {
+		return matches
+	}
+
+	r := key[index]
+	isWild := r == '*' || r == '.'
+
+	if isWild || r < n.r {
+		matches = append(matches, t.wildcardRecursive(n.small, key, index, prefix)...)
+	}
+	if isWild || r > n.r {
+		matches = append(matches, t.wildcardRecursive(n.large, key, index, prefix)...)
+	}
+	if isWild || r == n.r {
+		newPrefix := prefix + string(n.r)
+		if n.end {
+			matches = append(matches, newPrefix)
+		}
+		matches = append(matches, t.wildcardRecursive(n.equal, key, index+1, newPrefix)...)
+	}
+
+	return matches
 }
 
 func (t *Trie) getRecursive(n *node, key []rune, index int) *node {
--- trie_test.go
+++ trie_test.go
@@ -1,14 +1,61 @@
 package trie
 
 import (
+	"fmt"
 	. "github.com/sdegutis/go.bdd"
 	"testing"
 )
+
+func ExampleTrie_Wildcard() {
+	t := &Trie{}
+	t.Put("Hello", "World")
+	t.Put("Hilly", "World")
+	t.Put("Hello, bob", "World")
+	t.Wildcard("H*ll.") // []string{"Hello", "Hilly"}
+	t.Wildcard("Hel")   // []string(nil)
+}
+
+func ExampleTrie_LongestPrefix() {
+	t := &Trie{}
+	t.Put("Hello", "World")
+	t.Put("Hello, brother", "World")
+	t.Put("Hello, bob", "World")
+	t.LongestPrefix("Hello, brandon") // "Hello"
+	t.LongestPrefix("Hel")            // ""
+	t.LongestPrefix("Hello")          // "Hello"
+}
+
+func ExampleTrie_Get() {
+	t := &Trie{}
+	t.Put("hello", "world")
+	fmt.Println(t.Get("hello").(string))
+	fmt.Println(t.Get("non-existant"))
+	// Output:
+	// world
+	// <nil>
+}
+
+func ExampleTrie_Put() {
+	t := &Trie{}
+	t.Put("hello", "world")
+	t.Put("hello", "world") // does the same thing
+	fmt.Println(t.Get("hello"))
+	t.Put("1", 1)
+	fmt.Println(t.Get("1").(int))
+	// Output:
+	// world
+	// 1
+}
 
 func TestEverything(t *testing.T) {
 	defer PrintSpecReport()
 
 	Describe("Trie", func() {
+		It("Returns nil when a key doesn't exist", func() {
+			trie := &Trie{}
+			Expect(trie.Get("foo"), ToEqual, nil)
+		})
+
 		It("Stores anything by its key", func() {
 			trie := &Trie{}
 
@@ -21,6 +68,26 @@ func TestEverything(t *testing.T) {
 			trie.Put("foobar", 21)
 			Expect(trie.Get("foobar").(int), ToEqual, 21)
 			Expect(trie.Get("foo").(int), ToEqual, 42)
+		})
+
+		trie := &Trie{}
+		trie.Put("Hello", "World")
+		trie.Put("Hello", "World")
+		trie.Put("Hilly", "World")
+		trie.Put("Hello, brother", "World")
+		trie.Put("Hello, bob", "World")
+
+		It("Matches wildcards", func() {
+			var s []string
+			Expect(trie.Wildcard("H*ll."), ToDeepEqual, []string{"Hilly", "Hello"})
+			Expect(trie.Wildcard("Hel"), ToDeepEqual, s)
+		})
+
+		It("Returns the longest prefix", func() {
+			Expect(trie.LongestPrefix("Hello, brandon"), ToEqual, "Hello")
+			Expect(trie.LongestPrefix("Hel"), ToEqual, "")
+			Expect(trie.LongestPrefix("Hello"), ToEqual, "Hello")
+			Expect(trie.LongestPrefix("Hello, bob"), ToEqual, "Hello, bob")
 		})
 	})
 }
